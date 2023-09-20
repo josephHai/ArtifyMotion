4bcf28d8998644c39b936b4e4d984773bcfbf3f0
JosephHai <junyan0803@gmail.com>
2023-09-20 17:47:30 +0800

    feat:路由更新（一期）

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -9,12 +9,50 @@ const routes: RouteRecordRaw[] = [
    * 首页
    */
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: () => import('@/views/home.vue'),
     meta: {
       title: 'Home',
     },
+  },
+  {
+    path: '/',
+    name: 'ArtifyMotion',
+    component: () => import('@/views/artifyMotion/index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'list',
+        component: () => import('@/views/artifyMotion/list.vue'),
+      },
+      {
+        path: 'upload',
+        component: () => import('@/views/artifyMotion/upload/index.vue'),
+        children: [
+          {
+            path: '',
+            name: 'upload',
+            component: () => import('@/views/artifyMotion/upload/upload.vue'),
+          },
+          {
+            path: 'finalize',
+            name: 'finalize',
+            component: () => import('@/views/artifyMotion/upload/finalize.vue'),
+          },
+        ],
+      },
+      {
+        path: 'create/replace/:id',
+        name: 'replace',
+        component: () => import('@/views/artifyMotion/creation.vue'),
+      },
+      {
+        path: 'create/edit/:id',
+        name: 'edit',
+        component: () => import('@/views/artifyMotion/edit.vue'),
+      },
+    ],
   },
 ]
 

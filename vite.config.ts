e567357d3ddcc7e95d13d295a7473f37609d760b
JosephHai <junyan0803@gmail.com>
2023-09-20 17:27:19 +0800

    build:添加自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoImport from 'unplugin-auto-import/vite'
 import components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import banner from 'vite-plugin-banner'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { envDir, sourceDir, manualChunks } from './scripts/build'
+// @ts-ignore
 import pkg from './package.json'
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import svgLoader from 'vite-svg-loader'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -67,6 +72,9 @@ export default defineConfig(({ mode }) => {
           manualChunks,
         },
       },
+      commonjsOptions: {
+        requireReturnsDefault: 'namespace',
+      },
     },
 
     resolve: {
@@ -123,10 +131,33 @@ export default defineConfig(({ mode }) => {
       vue(),
 
       /**
+       * 添加svgLoader插件
+       */
+      svgLoader(),
+
+      /**
        * 如果需要支持 `.tsx` 组件，请安装 `@vitejs/plugin-vue-jsx` 这个包
        * 并在这里添加一个插件导入 `import vueJsx from '@vitejs/plugin-vue-jsx'`
        */
       // vueJsx(),
+      autoImport({
+        // Auto import functions from Vue, e.g. ref, reactive, toRef...
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue', 'vue-router', 'pinia'],
+
+        // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        resolvers: [
+          ElementPlusResolver(),
+
+          // Auto import icon components
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+        dts: path.resolve(sourceDir, 'auto-imports.d.ts'),
+      }),
 
       /**
        * 自动导入组件，不用每次都 import
@@ -140,10 +171,20 @@ export default defineConfig(({ mode }) => {
         extensions: ['vue', 'ts', 'tsx'],
         deep: true,
         dts: 'src/types/declaration-files/components.d.ts',
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+          // Auto register icon components
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),
+          // Auto register Element Plus components
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+        ],
       }),
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
+
+      Icons({
+        autoInstall: true,
       }),
 
       /**
@@ -176,5 +217,12 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+
+    // 消除i18n的警告信息: It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
+    define: {
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: false,
+      __INTLIFY_PROD_DEVTOOLS__: false,
+    },
   }
 })

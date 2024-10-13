5527d5afc0ef8234170151976ac160e6d2695be4
josephHai <junyan0803@gmail.com>
2024-10-13 21:45:51 +0800

    style: 小加载图标样式修改

--- src/assets/icon/loaders/index.ts
+++ src/assets/icon/loaders/index.ts
@@ -1,6 +1,6 @@
 import IconLoading from './icon-loading.vue'
 import LocalLoadingSvg from './local-loading.svg?raw'
 import LocalLoading from './local-loading.svg'
-import TinyLoading from './tiny-loading.svg'
+import TinyLoading from './tiny-loading.vue'
 
 export { IconLoading, LocalLoadingSvg, LocalLoading, TinyLoading }
